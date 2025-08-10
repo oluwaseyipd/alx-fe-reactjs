417f3ec210e38589aeb71ddb008eccf031ce0c97
Oluwaseyi <oluwaseyipd@gmail.com>
2025-08-10 16:31:33 +0100

    1. Styling a Provided React Component with Tailwind CSS

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+
+function App() {
+
+
+  return (
+    <>
+      <UserProfile />
+    </>
+  )
+}
+
+export default App
