7f8196400bdd278fa94624765d22440db3d4f3b1
Oluwaseyi <oluwaseyipd@gmail.com>
2025-08-23 08:53:18 +0100

    3. Implementing and Testing a Todo List Component in React

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText("Add a new todo");
+    fireEvent.change(input, { target: { value: "New Task" } });
+    fireEvent.click(screen.getByText("Add"));
+
+    expect(screen.getByText("New Task")).toBeInTheDocument();
+  });
+
+  test("toggles a todo's completion", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    fireEvent.click(todo);
+
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Build a Todo App");
+    const deleteButton = todo.querySelector("button");
+    fireEvent.click(deleteButton);
+
+    expect(todo).not.toBeInTheDocument();
+  });
+});
